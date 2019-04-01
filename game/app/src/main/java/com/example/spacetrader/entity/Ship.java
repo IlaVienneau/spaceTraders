3f7e872dfd9ad067ef3f3ea1fcbec69016cd0275
clong71 <46767039+clong71@users.noreply.github.com>
2019-03-31 22:17:01 -0400

    still working on ship

--- game/app/src/main/java/com/example/spacetrader/entity/Ship.java
+++ game/app/src/main/java/com/example/spacetrader/entity/Ship.java
@@ -17,43 +17,63 @@ public class Ship implements Serializable {
         switch(type) {
             case FLEA:
                 capacity = 0;   //not sure how many it actually has, just says few in centre
+                currCapacity = 0;
                 fuel = 20;
+                currFuel = 20;
                 break;
             case GNAT:
                 capacity = 15;
                 fuel = 14;
+                currCapacity = 15;
+                currFuel = 14;
                 break;
             case WASP:
                 capacity = 35;
                 fuel = 14;
+                currCapacity = 35;
+                currFuel = 14;
                 break;
             case BEETLE:
                 capacity = 50;
                 fuel = 14;
+                currCapacity = 50;
+                currFuel = 14;
                 break;
             case HORNET:
                 capacity = 20;
                 fuel = 16;
+                currCapacity = 20;
+                currFuel = 16;
                 break;
             case FIREFLY:
                 capacity = 20;
                 fuel = 17;
+                currCapacity = 20;
+                currFuel = 17;
                 break;
             case TERMITE:
                 capacity = 60;
                 fuel = 14;
+                currCapacity = 60;
+                currFuel = 14;
                 break;
             case MOSQUITO:
                 capacity = 15;
                 fuel = 13;
+                currCapacity = 15;
+                currFuel = 13;
                 break;
             case BUMBLEBEE:
                 capacity = 20;
                 fuel = 15;
+                currCapacity = 20;
+                currFuel = 15;
                 break;
             case GRASSHOPPER:
                 capacity = 30;
                 fuel = 15;
+                currCapacity = 30;
+                currFuel = 15;
         }
     }
 
