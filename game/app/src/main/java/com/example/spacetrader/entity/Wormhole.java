b39fcc94469cb21b38b40c7f54e50466790f32c2
ProblemFree <36163330+ProblemFree@users.noreply.github.com>
2019-04-08 19:35:23 -0400

    Update Wormhole.java

--- game/app/src/main/java/com/example/spacetrader/entity/Wormhole.java
+++ game/app/src/main/java/com/example/spacetrader/entity/Wormhole.java
@@ -1,15 +1,10 @@
 package com.example.spacetrader.entity;
 import java.io.Serializable;
 
-<<<<<<< HEAD
-class Wormhole implements Serializable {
-    private Star startpoint;
-    private Star endpoint;
-=======
+
 public class Wormhole implements Serializable {
     private Star startPoint;
     private Star endPoint;
->>>>>>> master
     private int tax;
 
     public Wormhole(Star s1, Star s2, int t) {
