aa2ecc65c2035e0d40b4168c51ceb681a275ed0c
ProblemFree <36163330+ProblemFree@users.noreply.github.com>
2019-04-07 22:23:34 -0400

    Update PlanetInventory.java

--- game/app/src/main/java/com/example/spacetrader/entity/PlanetInventory.java
+++ game/app/src/main/java/com/example/spacetrader/entity/PlanetInventory.java
@@ -13,7 +13,11 @@ public class PlanetInventory implements Serializable {
 
     private HashMap<TradeGood, TradeStock> inventory;
     private TechLevel techLevel;
+    private PoliticalSystem politicalSystem;
+    private Resource resource;
+    private RadicalEvent event;
 
+<<<<<<< HEAD
     /**
     * creates inventory of trade goods for this planet based on technology level. It creates 10 of
      * each relevant trade goodo and updates the price.
@@ -21,8 +25,14 @@ public class PlanetInventory implements Serializable {
      * @param TechLevel tech: the TechLevel of this planet.
      */
     public PlanetInventory(TechLevel tech) {
+=======
+    public PlanetInventory(TechLevel tech, PoliticalSystem pol, Resource res, RadicalEvent event) {
+>>>>>>> William
         this.inventory = new HashMap<>();
         this.techLevel = tech;
+        this.politicalSystem = pol;
+        this.resource = res;
+        this.event = event;
 
         ArrayList<TradeGood> arr = new ArrayList<>();
         switch (tech) {
@@ -79,8 +89,23 @@ public class PlanetInventory implements Serializable {
             if (rand.nextInt(2) == 0) {
                 var *= -1;
             }
-
             price += var;
+            if (good.getIe() == this.event) {
+                price *= 5;
+            }
+            if (good.getCr() == this.resource) {
+                price /= 2;
+            }
+            if (good.getEr() == this.resource) {
+                price *= 2;
+            }
+
+            price = (int) Math.ceil((double) price);
+
+            if (price <= 0) {
+                price = rand.nextInt(10) + 1;
+            }
+
             stock.price = price;
         }
     }
